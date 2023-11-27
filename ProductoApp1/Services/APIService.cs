39c63221c70976ac23d8b84ddd9d42783c40e12c
Camila Isabel Sánchez <camila.sanchez.urresta@udla.edu.ec>
2023-11-27 16:17:32 -0500

    Nuevo Ejemplo

--- ProductoApp1/Services/APIService.cs
+++ ProductoApp1/Services/APIService.cs
@@ -18,7 +18,7 @@ namespace ProductoApp1.Services
         public APIService()
         {
 
-            _baseUrl = "https://apiproductos20231127081334.azurewebsites.net";
+            _baseUrl = "https://apiproductos20231127081335.azurewebsites.net";
             _httpClient = new HttpClient();
             _httpClient.BaseAddress = new Uri(_baseUrl);
         }
